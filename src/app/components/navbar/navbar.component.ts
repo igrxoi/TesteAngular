e9a9d4b3698e91684ceb23861881656ec6ead50d
igrxoi <105753802+igrxoi@users.noreply.github.com>
2022-06-07 21:36:38 -0300

    feat: Responsive design.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faCommentAlt } from '@fortawesome/free-solid-svg-icons';
+import { faClipboard } from '@fortawesome/free-solid-svg-icons';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -10,10 +18,22 @@ import { faBell } from '@fortawesome/free-solid-svg-icons';
 export class NavbarComponent implements OnInit {
 
   public faBell: any = faBell;
+  public faBars: any = faBars;
+  public faSearch: any = faSearch;
+  public faStar: any = faStar;
+  public faCommentAlt: any = faCommentAlt;
+  public faClipboard: any = faClipboard;
+  public faHouse: any = faHouse;
+  public faXmark: any = faXmark;
+  public showMenu: boolean = false;
   
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openMenu(): void {
+    this.showMenu = this.showMenu ? false : true;
   }
 
 }
